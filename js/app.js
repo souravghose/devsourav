525ab9f2fb7c3c6ce9d4fb9db6308977c364a0c6
souravghose <mailofsourav998@gmail.com>
2021-12-01 17:16:12 +0600

    popup developed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 $('.counter').countUp();
+
+// popup time interval
+let popUpMsg = document.getElementById('popUpMessage');
+setInterval(function() {
+    popUpMsg.style.visibility = 'visible';
+}, 3000)
+
+// popup close function
+let popUp = document.getElementById('popUpMessage');
+let btn = document.getElementById('close');
+btn.addEventListener('click', closePopUP);
+
+function closePopUP(e) {
+
+    e.target.parentNode.remove(popUp);
+}
